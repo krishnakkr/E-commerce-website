c654d2f0a5ec556f4152bc31d1326d5716d17f4c
kakarla sai krishna <krishnakkr9999@gmail.com>
2023-05-17 01:37:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors';
 import path from 'path';
+import {fileURLToPath} from 'url';
 
 // conifgure env
 dotenv.config();
 //database config
 connectDB();
+// esmodule fix
+const __filename=fileUrltoPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 // rest object
 const app = express();
 //middlewares
